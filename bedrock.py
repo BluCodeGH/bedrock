8fbe571b9284633c128219401d245ab0eaaa8dc3
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2018-08-26 22:05:57 -0400

    Initial commit

---
+++ bedrock.py
@@ -0,0 +1,285 @@
+# Interprets the minecraft bedrock world format.
+
+import struct
+import os.path
+import numpy as np
+import bedrock.leveldb as ldb
+import bedrock.nbt as nbt
+
+# Handles chunk loading and mapping blocks to chunks.
+class World:
+  def __init__(self, path):
+    self.path = os.path.join(path, "db")
+    self.db = None
+    self.chunks = {}
+
+  # Enable use in a with statement.
+  def __enter__(self):
+    self.db = ldb.open(self.path)
+    return self
+
+  def __exit__(self, *args):
+    self.save()
+    ldb.close(self.db)
+    return False
+
+  def getChunk(self, x, z):
+    chunk = self.chunks.get((x, z), None)
+    if chunk is None:
+      chunk = Chunk(self.db, x, z)
+      self.chunks[(x, z)] = chunk
+    return chunk
+
+  def getBlock(self, x, y, z):
+    cx = x // 16
+    x %= 16
+    cz = z // 16
+    z %= 16
+    chunk = self.getChunk(cx, cz)
+    return chunk.getBlock(x, y, z)
+
+  def setBlock(self, x, y, z, block):
+    cx = x // 16
+    x %= 16
+    cz = z // 16
+    z %= 16
+    chunk = self.getChunk(cx, cz)
+    return chunk.setBlock(x, y, z, block)
+
+  def save(self):
+    for chunk in self.chunks.values():
+      chunk.save(self.db)
+
+# Handles biomes and tile entities. Maps blocks to subchunks.
+class Chunk:
+  def __init__(self, db, x, z):
+    self.x = x
+    self.z = z
+    # Leveldb chunks are stored in a number of keys with the same prefix.
+    self.keyBase = struct.pack("<ii", self.x, self.z)
+
+    self.version = self._loadVersion(db)
+    self.hMap, self.biomes = self._load2D(db)
+
+    self.subchunks = []
+    for i in range(16):
+      try:
+        self.subchunks.append(SubChunk(db, self.x, self.z, i)) #Pass off processing to the subchunk class
+      #All the subchunks below an existing subchunk also exist, so we know we have reached the end.
+      except KeyError:
+        break
+
+    self._loadTileEntities(db)
+
+  # Version is simply a stored value.
+  def _loadVersion(self, db):
+    try:
+      version = ldb.get(db, self.keyBase + b"v")
+      version = struct.unpack("<B", version)[0]
+      if version != 7:
+        raise NotImplementedError("Unexpected chunk version {}.".format(version))
+    except KeyError:
+      raise ValueError("Chunk at {}, {} does not exist.".format(self.x, self.z))
+    return version
+
+  # Load heightmap (seemingly useless) and biome info
+  def _load2D(self, db):
+    data = ldb.get(db, self.keyBase + b'-')
+    heightMap = struct.unpack("<" + "H" * 16 * 16, data[:2 * 16 * 16])
+    biomes = struct.unpack("B" * 16 * 16, data[2 * 16 * 16:])
+    return heightMap, biomes
+
+  # Tile entities are stored as a bunch of NBT compound tags end to end.
+  def _loadTileEntities(self, db):
+    try:
+      data = ldb.get(db, self.keyBase + b"1")
+    except KeyError:
+      return
+    data = nbt.DataReader(data)
+    while not data.finished():
+      nbt = nbt.decode(data)
+      x = nbt.pop("x").payload # We add back theses with the correct value on save, they are important.
+      y = nbt.pop("y").payload
+      z = nbt.pop("z").payload
+      self.getBlock(x % 16, y, z % 16).nbt = nbt
+
+  def getBlock(self, x, y, z):
+    if y // 16 + 1 > len(self.subchunks):
+      return None
+    return self.subchunks[y // 16].getBlock(x, y % 16, z)
+
+  def setBlock(self, x, y, z, block):
+    while y // 16 + 1 > len(self.subchunks):
+      self.subchunks.append(SubChunk.empty(self.x, self.z, len(self.subchunks)))
+    self.subchunks[y // 16].setBlock(x, y % 16, z, block)
+
+  def save(self, db):
+    version = struct.pack("<B", self.version)
+    ldb.put(db, self.keyBase + b"v", version)
+    self._save2D(db)
+    for subchunk in self.subchunks:
+      subchunk.save(db)
+    self._saveTileEntities(db)
+
+  def _save2D(self, db):
+    pass
+
+  def _saveTileEntities(self, db):
+    data = nbt.DataWriter()
+    for subchunk in self.subchunks:
+      for x in range(16):
+        for y in range(16):
+          for z in range(16):
+            block = subchunk.getBlock(x, y, z)
+            if block.nbt is not None: # Add back the correct position.
+              block.nbt.add(nbt.TAG_Int("x", self.x * 16 + x))
+              block.nbt.add(nbt.TAG_Int("y", y))
+              block.nbt.add(nbt.TAG_Int("z", self.z * 16 + z))
+              nbt.encode(block.nbt, data)
+    ldb.put(db, self.keyBase + b"1", data.data)
+
+  def __repr__(self):
+    return "Chunk {} {}: {} subchunks".format(self.x, self.z, len(self.subchunks))
+
+# Handles the blocks and block palette format.
+class SubChunk:
+  def __init__(self, db, x, z, y):
+    self.x = x
+    self.z = z
+    self.y = y
+    if db is not None: # For creating subchunks, there will be no DB.
+      # Subchunks are stored as base key + subchunk key `/` + subchunk id (y level // 16)
+      key = struct.pack("<iicB", x, z, b'/', y)
+      data = ldb.get(db, key)
+      self.version, data = data[0], data[1:]
+      if self.version != 8:
+        raise NotImplementedError("Unsupported subchunk version: {}".format(self.version))
+      numStorages, data = data[0], data[1:]
+      if numStorages != 1: # Maybe used for liquids
+        raise NotImplementedError("Unexpected number of storages: {}".format(numStorages))
+
+      blocks, data = self._loadBlocks(data)
+      palette = self._loadPalette(data)
+
+      self.blocks = np.empty(4096, dtype=Block) # Prepare with correct dtype
+      for i, block in enumerate(blocks):
+        block = palette[block]
+        self.blocks[i] = Block(block["name"].payload, block["val"].payload) # .payload to get actual val
+
+      self.blocks = self.blocks.reshape(16, 16, 16).swapaxes(1, 2) # Y and Z saved in an inverted order
+
+  # These arent actual blocks, just ids pointing to the palette.
+  def _loadBlocks(self, data):
+    #Ignore LSB of data (its a flag) and get compacting level
+    bitsPerBlock, data = data[0] >> 1, data[1:]
+    blocksPerWord = 32 // bitsPerBlock # Word = 4 bytes, basis of compacting.
+    numWords = - (-4096 // blocksPerWord) # Ceiling divide is inverted floor divide
+
+    blockWords, data = struct.unpack("<" + "I" * numWords, data[:4 * numWords]), data[4 * numWords:]
+    blocks = np.empty(4096, dtype=np.uint32)
+    for i, word in enumerate(blockWords):
+      for j in range(blocksPerWord):
+        block = word & ((1 << bitsPerBlock) - 1) # Mask out number of bits for one block
+        word >>= bitsPerBlock # For next iteration
+        if i * blocksPerWord + j < 4096: # Safety net for padding at end.
+          blocks[i * blocksPerWord + j] = block
+    return blocks, data
+
+  # NBT encoded block names (with minecraft:) and data values.
+  def _loadPalette(self, data):
+    palletLen, data = struct.unpack("<I", data[:4])[0], data[4:]
+    dr = nbt.DataReader(data)
+    palette = []
+    for _ in range(palletLen):
+      palette.append(nbt.decode(dr))
+    return palette
+
+  def getBlock(self, x, y, z):
+    return self.blocks[x,y,z]
+
+  def setBlock(self, x, y, z, block):
+    self.blocks[x,y,z] = block
+
+  def save(self, db):
+    data = struct.pack("<BB", self.version, 1)
+    palette, blockIDs = self._savePalette()
+    data += self._saveBlocks(len(palette), blockIDs)
+    data += struct.pack("<I", len(palette))
+    for block in palette:
+      data += nbt.encode(block)
+
+    key = struct.pack("<iicB", self.x, self.z, b'/', self.y)
+    ldb.put(db, key, data)
+
+  # Compact blockIDs bitwise. See _loadBlocks for details.
+  def _saveBlocks(self, paletteSize, blockIDs):
+    bitsPerBlock = max(int(np.ceil(np.log2(paletteSize))), 1)
+    blocksPerWord = 32 // bitsPerBlock
+    numWords = - (-4096 // blocksPerWord)
+    data = struct.pack("<B", bitsPerBlock << 1)
+
+    for i in range(numWords):
+      word = 0
+      for j in range(blocksPerWord - 1, -1, -1):
+        if i * blocksPerWord + j < 4096:
+          word <<= bitsPerBlock
+          word |= blockIDs[i * blocksPerWord + j]
+      data += struct.pack("<I", word)
+    return data
+
+  # Make a palette, and get the block ids at the same time
+  def _savePalette(self):
+    blocks = self.blocks.swapaxes(1, 2).reshape(4096) # Y and Z saved in a inverted order
+    blockIDs = np.empty(4096, dtype=np.uint32)
+    palette = []
+    for i, block in enumerate(blocks):
+      # Generate the palette nbt for the given block
+      block = nbt.TAG_Compound("", [nbt.TAG_String("name", block.name), nbt.TAG_Short("val", block.dv)])
+      if block not in palette:
+        palette.append(block)
+      blockIDs[i] = palette.index(block)
+    return palette, blockIDs
+
+  @classmethod
+  def empty(cls, x, z, y):
+    subchunk = cls(None, x, z, y)
+    subchunk.version = 8
+    subchunk.blocks = np.full((16, 16, 16), Block("minecraft:air"), dtype=Block)
+    return subchunk
+
+# Generic block storage.
+class Block:
+  def __init__(self, name, dv=0, nbt=None):
+    self.name = name
+    self.dv = dv
+    self.nbt = nbt
+
+  def __repr__(self):
+    return "{} {}".format(self.name, self.dv)
+
+# Handles NBT generation for command blocks.
+class CommandBlock(Block):
+  nameMap = {"I": "command_block", "C": "chain_command_block", "R": "repeating_command_block"}
+  dMap = {"d": 0, "u": 1, "-z": 2, "+z": 3, "-x": 4, "+x": 5}
+  def __init__(self, cmd="", hover="", block="I", d="u", cond=False, redstone=False):
+    name = "minecraft:" + self.nameMap[block]
+    dv = self.dMap[d]
+    if cond:
+      dv += 8
+    nbt = nbt.TAG_Compound("", [])
+    nbt.add(nbt.TAG_String("Command", cmd))
+    nbt.add(nbt.TAG_String("CustomName", hover))
+    nbt.add(nbt.TAG_Byte("TrackOutput", 1))
+    nbt.add(nbt.TAG_Int("Version", 8))
+    nbt.add(nbt.TAG_Byte("auto", int(not redstone)))
+    nbt.add(nbt.TAG_String("id", "CommandBlock"))
+    nbt.add(nbt.TAG_Byte("isMovable", 1))
+    nbt.add(nbt.TAG_Byte("powered", 0))
+    nbt.add(nbt.TAG_Int("LPCommandMode", 0)) # Not sure what these LPModes do. This works.
+    nbt.add(nbt.TAG_Byte("LPConditionalMode", 0))
+    nbt.add(nbt.TAG_Byte("LPRedstoneMode", 0))
+    nbt.add(nbt.TAG_Long("LastExecution", 0))
+    nbt.add(nbt.TAG_String("LastOutput", ""))
+    nbt.add(nbt.TAG_List("LastOutputParams", [nbt.TAG_String(n, str(n)) for n in range(3)]))
+    nbt.add(nbt.TAG_Int("SuccessCount", 0))
+    super().__init__(name, dv, nbt)
---
+++ nbt.py
@@ -0,0 +1,186 @@
+# A simple and flexible NBT parser.
+
+import struct
+
+# Allows for easy sequential reading of binary data
+class DataReader:
+  def __init__(self, data):
+    self.data = data
+    self.idx = 0
+
+  def pop(self, size):
+    fmt = [None, "b", "h", None, "i", None, None, None, "q"]
+    popped = struct.unpack("<{}".format(fmt[size]), self.data[self.idx:self.idx + size])[0]
+    self.idx += size
+    return popped
+
+  # Specific to the NBT string format, two bytes for size followed by that many bytes of string.
+  def popString(self):
+    size = self.pop(2)
+    popped = struct.unpack("<{}s".format(size), self.data[self.idx:self.idx + size])[0]
+    self.idx += size
+    return popped.decode("utf-8")
+
+  # Useful when dealing with an unknown number of compound tags back to back.
+  def finished(self):
+    return self.idx >= len(self.data)
+
+  def __repr__(self):
+    return self.data[self.idx:]
+
+# Allows for easy sequential writing of binary data.
+class DataWriter:
+  def __init__(self):
+    self.data = b""
+
+  def put(self, size, *data):
+    fmt = [None, "b", "h", None, "i", None, None, None, "q"]
+    self.data += struct.pack("<{}".format(fmt[size]), *data)
+
+  def putString(self, string):
+    self.put(2, len(string))
+    self.data += struct.pack("<{}s".format(len(string)), string.encode("utf-8"))
+
+  def __repr__(self):
+    return self.data
+
+tags = [None for _ in range(13)]
+
+# Generic base tag, calls self.decode with binary data to fill in payload.
+class TAG:
+  ID = None
+  def __init__(self, name, data):
+    self.name = name
+    if isinstance(data, DataReader):
+      self.payload = self.decode(data)
+    else:
+      self.payload = data
+
+  def decode(self, data):
+    raise NotImplementedError("Decode method not overridden by subclass.")
+
+  def __getitem__(self, name):
+    for item in self.payload:
+      if item.name == name:
+        return item
+    raise KeyError("{} not found in {}".format(name, self.payload))
+
+  def __eq__(self, other):
+    return self.name == other.name and self.payload == other.payload and self.ID == other.ID
+
+  def __repr__(self):
+    return "{}-{}:{}".format(self.__class__.__name__, self.name, self.payload)
+
+class TAG_Byte(TAG):
+  ID = 1
+  def decode(self, data):
+    return data.pop(1)
+
+  def encode(self, data):
+    data.put(1, self.payload)
+tags[1] = TAG_Byte
+
+class TAG_Short(TAG):
+  ID = 2
+  def decode(self, data):
+    return data.pop(2)
+
+  def encode(self, data):
+    data.put(2, self.payload)
+tags[2] = TAG_Short
+
+class TAG_Int(TAG):
+  ID = 3
+  def decode(self, data):
+    return data.pop(4)
+
+  def encode(self, data):
+    data.put(4, self.payload)
+tags[3] = TAG_Int
+
+class TAG_Long(TAG):
+  ID = 4
+  def decode(self, data):
+    return data.pop(8)
+  def encode(self, data):
+    data.put(8, self.payload)
+tags[4] = TAG_Long
+
+class TAG_String(TAG):
+  ID = 8
+  def decode(self, data):
+    return data.popString()
+  def encode(self, data):
+    data.putString(self.payload)
+tags[8] = TAG_String
+
+# Basically a TAG_Compound, but the items don't have names, and instead are named integer indexes.
+#  This allows for a generic __getitem__ function in the TAG class.
+class TAG_List(TAG):
+  ID = 9
+  def decode(self, data):
+    self.itemID = data.pop(1)
+    size = data.pop(4)
+    payload = []
+    for i in range(size):
+      payload.append(tags[self.itemID](i, data))
+    return payload
+
+  def encode(self, data):
+    if self.payload == []: # We don't know the data type.
+      data.put(1, 0) # Default to TAG_End
+    else:
+      data.put(1, tags.index(type(self.payload[0])))
+    data.put(4, len(self.payload))
+    for item in self.payload:
+      item.encode(data)
+
+  def add(self, tag):
+    self.payload.append(tag)
+tags[9] = TAG_List
+
+# Stores some number of complete tags, followed by a TAG_End
+class TAG_Compound(TAG):
+  ID = 10
+  def decode(self, data):
+    payload = []
+    tagID = data.pop(1)
+    while tagID != 0:
+      if tags[tagID] is not None:
+        name = data.popString()
+        payload.append(tags[tagID](name, data))
+      else:
+        raise NotImplementedError("Tag {} not implemented.".format(tagID))
+      tagID = data.pop(1)
+    return payload
+
+  def encode(self, data):
+    for item in self.payload:
+      data.put(1, item.ID)
+      data.putString(item.name)
+      item.encode(data)
+    data.put(1, 0)
+
+  def add(self, tag):
+    self.payload.append(tag)
+
+  def pop(self, name):
+    for i in range(len(self.payload)):
+      if self.payload[i].name == name:
+        return self.payload.pop(i)
+    return None
+tags[10] = TAG_Compound
+
+def decode(data):
+  tagID = data.pop(1)
+  if tags[tagID] is not None:
+    name = data.popString()
+    return tags[tagID](name, data)
+  raise NotImplementedError("Tag {} not implemented.".format(tagID))
+
+def encode(toEncode, data=None):
+  data = data or DataWriter()
+  data.put(1, toEncode.ID)
+  data.putString(toEncode.name)
+  toEncode.encode(data)
+  return data.data
